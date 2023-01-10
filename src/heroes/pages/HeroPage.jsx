17996dfca29dc0b31aee558a5d23dab02d42d5fa
Lisperguer <felipe.lisperguer.h@gmail.com>
2023-01-10 17:56:21 -0300

    feat(HeroesReact): add queryparams search

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,14 +1,23 @@
 import { useMemo } from "react";
-import { Navigate, useNavigate, useParams } from "react-router-dom";
+import {
+  Navigate,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
 import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
   const navigate = useNavigate();
   const { id } = useParams();
-  const hero = useMemo(() => getHeroById(id), [id]);
+  const hero = useMemo(
+    () => getHeroById(id),
+    [id]
+  );
 
   const onNavigateBack = () => {
-    hero.publisher === "DC Comics" ? navigate("/dc") : navigate("/marvel");
+    hero.publisher === "DC Comics"
+      ? navigate("/dc")
+      : navigate("/marvel");
   };
 
   if (!hero) {
@@ -32,20 +41,21 @@ export const HeroPage = () => {
             <b> Alter ego:</b> {hero.alter_ego}
           </li>
           <li className="list-group-item">
-            <b> Publisher:</b> {hero.publisher}
+            <b>Publisher:</b> {hero.publisher}
           </li>
           <li className="list-group-item">
-            <b> First appearence:</b> {hero.first_appearance}
+            <b>First appearence:</b>{" "}
+            {hero.first_appearance}
           </li>
         </ul>
         <h5 className="mt-3"> Characters</h5>
         <p>{hero.characters}</p>
-        <button className="btn btn-outline-primary" onClick={onNavigateBack}>
-          {" "}
+        <button
+          className="btn btn-outline-primary"
+          onClick={onNavigateBack}>
           Regresar
         </button>
       </div>
     </div>
   );
-  
 };
